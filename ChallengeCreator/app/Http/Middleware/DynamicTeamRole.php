c30d051f6d99077103d7b89fb291225eb425c6ff
VDT1805 <136230459+VDT1805@users.noreply.github.com>
2024-02-04 21:33:42 +0700

    QuestionBank -cs + DynamicRoleMiddleware

---
+++ ChallengeCreator/app/Http/Middleware/DynamicTeamRole.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Laratrust\Middleware\LaratrustMiddleware;
+use Laratrust\Middleware\Role;
+use Symfony\Component\HttpFoundation\Response;
+
+class DynamicTeamRole extends LaratrustMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(
+    Request $request,
+    Closure $next,
+    string|array $roles,
+    ?string $options = ''): Response
+    {
+        $team_id = $request->route('id'); //Team id
+        if (! $this->authorization('roles', $roles, $team_id, $options)) {
+            return $this->unauthorized();
+        }
+
+        return $next($request);
+    }
+}
